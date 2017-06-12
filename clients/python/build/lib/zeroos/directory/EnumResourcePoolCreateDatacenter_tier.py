5f1683bd700787cff14d580f7a550e1b45bcaafc
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-06-12 15:13:39 +0200

    remove generated files

--- clients/python/build/lib/zeroos/directory/EnumResourcePoolCreateDatacenter_tier.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class EnumResourcePoolCreateDatacenter_tier(Enum):
-    _1 = "1"
-    _2 = "2"
-    _3 = "3"
-    _4 = "4"
--- clients/python/build/lib/zeroos/directory/EnumResourcePoolCreatePricing_currency.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class EnumResourcePoolCreatePricing_currency(Enum):
-    euro = "euro"
-    usb = "usb"
-    hr_index = "hr_index"
